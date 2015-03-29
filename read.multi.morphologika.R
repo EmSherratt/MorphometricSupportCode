f29f9d0b45f447bb21a59b898bc0d43f249f27b7
Emma Sherratt <EmSherratt@users.noreply.github.com>
2015-03-30 09:59:11 +1100

    Update read.multi.morphologika.R

--- read.multi.morphologika.R
+++ read.multi.morphologika.R
@@ -1,11 +1,11 @@
 # Source file to use with library(geomorph)
 # Emma Sherratt December 2014
-
+#
 # Code reads multiple morphologika files that each contain only a single specimen (such as can be exported from
 # Stratovan Checkpoint software https://www.stratovan.com/products/checkpoint
-
+#
 # To use, filelist <- list.files(pattern = "*.txt") # list all morpholgika files
-
+#
 read.multi.morphologika <- function(filelist){
   names <- gsub (".txt", "", filelist) # extracts names of specimens from the file name
   coords <- NULL # make empty object that will be filled with 3D array of coordinate data
