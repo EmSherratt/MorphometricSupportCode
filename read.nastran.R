68551821ae3c0675e6b8dcb19587eb24d7156c83
EmSherratt <emma.sherratt@gmail.com>
2015-07-14 15:01:06 +1000

    added trip and quad mesh in single file

--- read.nastran.R
+++ read.nastran.R
@@ -10,24 +10,30 @@ read.nastran <- function (file){
   GRIDline <- grep("GRID*", nasfile, value = FALSE)
   nnodes <- length(GRIDline)
   vertices <- matrix(unlist(strsplit(nasfile[GRIDline[1] : (GRIDline[nnodes]+1)], split = "\\s+")), ncol=6, byrow=T)
+  vert.adresses <- vertices[,2]
   vertices <- matrix(as.numeric(vertices[,c(3,4,6)]), ncol=3, byrow=F)
+  rownames(vertices) <- vert.adresses
   # If it's a Quad Mesh
+  qmesh <- NULL
   CTETRAline <- grep("CTETRA", nasfile, value = TRUE)
   if(length(CTETRAline)!=0){ paste("File is quad mesh")
     nbricks <- length(CTETRAline)
     indices <- matrix(unlist(strsplit(grep("CTETRA", nasfile, value = TRUE), 
                                       split = "\\s+")), ncol=7, byrow=T)
     indices <- matrix(as.numeric(indices[,c(4:7)]), ncol=4, byrow=F)
-    mesh <- qmesh3d(rbind(t(vertices), 1), indices)
-    }
+    qmesh <- qmesh3d(rbind(t(vertices), 1), indices)
+  }
   # If it's a Tri Mesh
+  tmesh <- NULL
   CTRIA3line <- grep("CTRIA3", nasfile, value = TRUE)
   if(length(CTRIA3line)!=0){ paste("File is triangular mesh")
     ntri <- length(CTRIA3line)
     indices <- matrix(unlist(strsplit(grep("CTRIA3", nasfile, value = TRUE), 
                                       split = "\\s+")), ncol=6, byrow=T)
     indices <- matrix(as.numeric(indices[,c(4:6)]), ncol=3, byrow=F)
-    mesh <- tmesh3d(rbind(t(vertices), 1), indices)
+    tmesh <- tmesh3d(rbind(t(vertices), 1), indices)
   }
-  return(mesh)
+  if(!is.null(tmesh) && !is.null(qmesh)){ return(list(tmesh = tmesh, qmesh = qmesh)) }
+  if(!is.null(qmesh)){ return(qmesh) }
+  if(!is.null(tmesh)){ return(tmesh) }
 }
